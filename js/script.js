e5ab308327fa1d7e562f8cde191cbe88a583a51e
nathanaeld01 <nathanael.dcunha@gmail.com>
2021-05-17 16:31:50 +0530

    Bug fixes

--- js/script.js
+++ js/script.js
@@ -40,9 +40,7 @@ $(document).ready(function() {
                 holi = true;
             }
         }
-        else {
-            checkTable();
-        }
+        checkTable();
     }
     function checkTable() {
         var date = new Date();
